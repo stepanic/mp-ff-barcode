dff71a6d30a6f15cb8dc47a0667a4bee1e8e8f15
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-06-28 14:52:29 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/custom_code/actions/share_file.dart
@@ -0,0 +1,15 @@
+// Automatic FlutterFlow imports
+import '/actions/actions.dart' as action_blocks;
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom actions
+import 'package:flutter/material.dart';
+// Begin custom action code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+Future<String> shareFile(
+  String barcodeType,
+  String barcodeData,
+) async {
+  return 'OK';
+}
