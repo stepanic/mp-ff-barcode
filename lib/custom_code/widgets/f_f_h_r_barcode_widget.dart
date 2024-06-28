3f000fcc4baa1ebb5f9a7bfa27b26db9ae46c52b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-06-28 10:25:37 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/custom_code/widgets/f_f_h_r_barcode_widget.dart
@@ -0,0 +1,30 @@
+// Automatic FlutterFlow imports
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom widgets
+import 'package:flutter/material.dart';
+// Begin custom widget code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+import 'package:barcode_widget/barcode_widget.dart';
+
+class FFHRBarcodeWidget extends StatefulWidget {
+  const FFHRBarcodeWidget({
+    super.key,
+    this.width,
+    this.height,
+  });
+
+  final double? width;
+  final double? height;
+
+  @override
+  State<FFHRBarcodeWidget> createState() => _FFHRBarcodeWidgetState();
+}
+
+class _FFHRBarcodeWidgetState extends State<FFHRBarcodeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
