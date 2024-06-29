309ac10164e0c8cb90a891ec6a0c1449efb87ddd
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-06-29 23:18:34 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/b_scan_barcode_page/b_scan_barcode_page_widget.dart
+++ lib/pages/b_scan_barcode_page/b_scan_barcode_page_widget.dart
@@ -24,6 +24,9 @@ class _BScanBarcodePageWidgetState extends State<BScanBarcodePageWidget> {
     super.initState();
     _model = createModel(context, () => BScanBarcodePageModel());
 
+    _model.barcodeDataTextController ??= TextEditingController();
+    _model.barcodeDataFocusNode ??= FocusNode();
+
     WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
   }
 
@@ -96,63 +99,161 @@ More about the official QR/Barcode Scanner you can find it [here](https://docs.f
                       onTapLink: (_, url, __) => launchURL(url!),
                     ),
                   ),
-                  FFButtonWidget(
-                    onPressed: () async {
-                      if (isWeb) {
-                        // show error snackbar
-                        ScaffoldMessenger.of(context).clearSnackBars();
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text(
-                              'Barcode Scanner is not supported on web platform',
-                              style: TextStyle(
-                                color: FlutterFlowTheme.of(context).primaryText,
+                  Padding(
+                    padding:
+                        const EdgeInsetsDirectional.fromSTEB(0.0, 36.0, 0.0, 36.0),
+                    child: FFButtonWidget(
+                      onPressed: () async {
+                        if (isWeb) {
+                          // show error snackbar
+                          ScaffoldMessenger.of(context).clearSnackBars();
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                'Barcode Scanner is not supported on web platform',
+                                style: TextStyle(
+                                  color:
+                                      FlutterFlowTheme.of(context).primaryText,
+                                ),
                               ),
+                              duration: const Duration(milliseconds: 4000),
+                              backgroundColor:
+                                  FlutterFlowTheme.of(context).error,
                             ),
-                            duration: const Duration(milliseconds: 4000),
-                            backgroundColor: FlutterFlowTheme.of(context).error,
-                          ),
-                        );
-                      } else {
-                        // start barcode scanner
-                        _model.barcodeSannerResult =
-                            await FlutterBarcodeScanner.scanBarcode(
-                          '#C62828', // scanning line color
-                          'Cancel', // cancel button text
-                          true, // whether to show the flash icon
-                          ScanMode.BARCODE,
-                        );
-                      }
+                          );
+                        } else {
+                          // reset form
+                          setState(() {
+                            _model.barcodeDataTextController?.clear();
+                          });
+                          // start barcode scanner
+                          _model.barcodeSannerResult =
+                              await FlutterBarcodeScanner.scanBarcode(
+                            '#C62828', // scanning line color
+                            'Cancel', // cancel button text
+                            true, // whether to show the flash icon
+                            ScanMode.BARCODE,
+                          );
 
-                      setState(() {});
-                    },
-                    text: 'Start Barcode Scanner',
-                    icon: const Icon(
-                      Icons.qr_code_scanner,
-                      size: 15.0,
-                    ),
-                    options: FFButtonOptions(
-                      width: double.infinity,
-                      height: 40.0,
-                      padding:
-                          const EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
-                      iconPadding:
-                          const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                      color: FlutterFlowTheme.of(context).primary,
-                      textStyle:
-                          FlutterFlowTheme.of(context).titleSmall.override(
-                                fontFamily: 'Readex Pro',
-                                color: Colors.white,
-                                letterSpacing: 0.0,
+                          if (_model.barcodeSannerResult != '') {
+                            // show scanner results in text field
+                            setState(() {
+                              _model.barcodeDataTextController?.text =
+                                  _model.barcodeSannerResult;
+                              _model.barcodeDataTextController?.selection =
+                                  TextSelection.collapsed(
+                                      offset: _model.barcodeDataTextController!
+                                          .text.length);
+                            });
+                          } else {
+                            // show error snackbar
+                            ScaffoldMessenger.of(context).clearSnackBars();
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                  'Some Scanner error, please try again.',
+                                  style: TextStyle(
+                                    color: FlutterFlowTheme.of(context)
+                                        .primaryText,
+                                  ),
+                                ),
+                                duration: const Duration(milliseconds: 4000),
+                                backgroundColor:
+                                    FlutterFlowTheme.of(context).error,
                               ),
-                      elevation: 3.0,
-                      borderSide: const BorderSide(
-                        color: Colors.transparent,
-                        width: 1.0,
+                            );
+                          }
+                        }
+
+                        setState(() {});
+                      },
+                      text: 'Start Barcode Scanner',
+                      icon: const Icon(
+                        Icons.qr_code_scanner,
+                        size: 15.0,
                       ),
-                      borderRadius: BorderRadius.circular(8.0),
+                      options: FFButtonOptions(
+                        width: double.infinity,
+                        height: 40.0,
+                        padding: const EdgeInsetsDirectional.fromSTEB(
+                            24.0, 0.0, 24.0, 0.0),
+                        iconPadding:
+                            const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                        color: FlutterFlowTheme.of(context).primary,
+                        textStyle:
+                            FlutterFlowTheme.of(context).titleSmall.override(
+                                  fontFamily: 'Readex Pro',
+                                  color: Colors.white,
+                                  letterSpacing: 0.0,
+                                ),
+                        elevation: 3.0,
+                        borderSide: const BorderSide(
+                          color: Colors.transparent,
+                          width: 1.0,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
                     ),
                   ),
+                  if (_model.barcodeDataTextController.text != '')
+                    TextFormField(
+                      controller: _model.barcodeDataTextController,
+                      focusNode: _model.barcodeDataFocusNode,
+                      autofocus: false,
+                      readOnly: true,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        labelText: 'Barcode Data',
+                        labelStyle:
+                            FlutterFlowTheme.of(context).labelMedium.override(
+                                  fontFamily: 'Readex Pro',
+                                  letterSpacing: 0.0,
+                                ),
+                        hintText: '???',
+                        hintStyle:
+                            FlutterFlowTheme.of(context).labelMedium.override(
+                                  fontFamily: 'Readex Pro',
+                                  letterSpacing: 0.0,
+                                ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: FlutterFlowTheme.of(context).alternate,
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: FlutterFlowTheme.of(context).primary,
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: FlutterFlowTheme.of(context).error,
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: FlutterFlowTheme.of(context).error,
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        filled: true,
+                      ),
+                      style: FlutterFlowTheme.of(context).bodyMedium.override(
+                            fontFamily: 'Readex Pro',
+                            letterSpacing: 0.0,
+                          ),
+                      maxLines: null,
+                      minLines: 3,
+                      validator: _model.barcodeDataTextControllerValidator
+                          .asValidator(context),
+                    ),
                 ]
                     .divide(const SizedBox(height: 20.0))
                     .addToStart(const SizedBox(height: 20.0))
